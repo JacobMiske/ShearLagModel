2f41e4efcb2cd6e7395cd42738d9a2ff1f530da6
Jacob Miske <jacobmiske@gmail.com>
2024-11-22 15:55:30 -0500

    plots

--- main.py
+++ main.py
@@ -60,24 +60,30 @@ class ShearLagComposite:
         x = np.linspace(-2,2,1000)
         interfacial_shear_stress = []
         for i in x:
-            interfacial_shear_stress_at_x = ((E_f*eps_1*n)/(2))*((np.sinh(n*s*i))/(np.cosh(n*s)))
+            interfacial_shear_stress_at_x = ((E_f*eps_1*n)/(1))*((np.sinh(n*s*i))/(np.cosh(n*s)))
             interfacial_shear_stress.append(interfacial_shear_stress_at_x)
         return x, interfacial_shear_stress
 
+    def get_modulus_of_composite(self):
+        """
+        """
+        E1 = self.volume_fraction*self.fiber_modulus*(1-(np.tanh(self.n_factor*self.s_factor)/(self.n_factor*self.s_factor)))+(1-self.volume_fraction)*self.matrix_modulus
+        return E1
 
+"""
 def plot_axial_stress(x, y, x_label, y_label):
-    """
-    """
     plt.plot(x, y)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     #plt.show()
     return 0
-
+"""
 
 p1a_material = ShearLagComposite()
 p1a_material.set_n_factor()
 
+
+# P1a
 p1a_material.s_factor = 2
 p1a_x, p1a_ax_stress_2 = p1a_material.get_axial_stress()
 p1a_material.s_factor = 5
@@ -92,7 +98,6 @@ p1a_material.s_factor = 40
 p1a_x, p1a_ax_stress_40 = p1a_material.get_axial_stress()
 p1a_material.s_factor = 50
 p1a_x, p1a_ax_stress_50 = p1a_material.get_axial_stress()
-
 plt.plot(p1a_x, p1a_ax_stress_2, label="s=2")
 plt.plot(p1a_x, p1a_ax_stress_5, label="s=5")
 plt.plot(p1a_x, p1a_ax_stress_10, label="s=10")
@@ -100,13 +105,15 @@ plt.plot(p1a_x, p1a_ax_stress_20, label="s=20")
 plt.plot(p1a_x, p1a_ax_stress_30, label="s=30")
 plt.plot(p1a_x, p1a_ax_stress_40, label="s=40")
 plt.plot(p1a_x, p1a_ax_stress_50, label="s=50")
-plt.xlabel("x")
+plt.xlabel("x/L")
 plt.ylabel("Axial Stress")
+plt.title("P1A")
 plt.legend()
 plt.savefig("./axial_stress_p1a.png")
 plt.show()
 
 
+# P1b
 p1a_material.s_factor = 2
 p1a_x, p1a_is_stress_2 = p1a_material.get_interfacial_shear_stress()
 p1a_material.s_factor = 5
@@ -121,7 +128,6 @@ p1a_material.s_factor = 40
 p1a_x, p1a_is_stress_40 = p1a_material.get_interfacial_shear_stress()
 p1a_material.s_factor = 50
 p1a_x, p1a_is_stress_50 = p1a_material.get_interfacial_shear_stress()
-
 plt.plot(p1a_x, p1a_is_stress_2, label="s=2")
 plt.plot(p1a_x, p1a_is_stress_5, label="s=5")
 plt.plot(p1a_x, p1a_is_stress_10, label="s=10")
@@ -129,19 +135,202 @@ plt.plot(p1a_x, p1a_is_stress_20, label="s=20")
 plt.plot(p1a_x, p1a_is_stress_30, label="s=30")
 plt.plot(p1a_x, p1a_is_stress_40, label="s=40")
 plt.plot(p1a_x, p1a_is_stress_50, label="s=50")
-plt.xlabel("x")
+plt.xlabel("x/L")
 plt.ylabel("Interfacial Shear Stress")
+plt.title("P1B")
 plt.legend()
-plt.savefig("./is_stress_p1a.png")
+plt.savefig("./is_stress_p1b.png")
+plt.show()
+
+
+# P1c
+# examine range of fiber modulus from 1 to 1000
+fiber_modulus = np.linspace(1e6, 1e9, 100)
+E_f_over_E_m = np.linspace(1,1000,100)
+p1a_material.s_factor = 5
+p1_mod_0p2 = []
+p1_mod_0p5 = []
+p1_mod_0p8 = []
+
+p1a_material.volume_fraction = 0.2
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p2.append( (p1a_material.get_modulus_of_composite())/(1000000) )
+
+p1a_material.volume_fraction = 0.5
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p5.append( (p1a_material.get_modulus_of_composite())/(1000000) )
+
+p1a_material.volume_fraction = 0.8
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p8.append( (p1a_material.get_modulus_of_composite())/(1000000) )
+
+plt.scatter(E_f_over_E_m, p1_mod_0p2, label="f=0.2")
+plt.scatter(E_f_over_E_m, p1_mod_0p5, label="f=0.5")
+plt.scatter(E_f_over_E_m, p1_mod_0p8, label="f=0.8")
+plt.xlabel("Fiber/Matrix Modulus Ratio")
+plt.ylabel("Composite/Matrix Modulus Ratio")
+plt.title("P1C")
+plt.legend()
+plt.savefig("./mod_ratio_p1c.png")
+plt.show()
+
+#P1d
+# examine range of fiber modulus from 1 to 1000
+fiber_modulus = np.linspace(1e6, 1e9, 100)
+E_f_over_E_m = np.linspace(1,1000,100)
+p1a_material.s_factor = 5
+p1_mod_0p2 = []
+p1_mod_0p5 = []
+p1_mod_0p8 = []
+
+p1a_material.volume_fraction = 0.2
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p2.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+p1a_material.volume_fraction = 0.5
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p5.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+p1a_material.volume_fraction = 0.8
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_mod_0p8.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+ax = plt.gca()
+ax.scatter(E_f_over_E_m, p1_mod_0p2, label="f=0.2")
+ax.scatter(E_f_over_E_m, p1_mod_0p5, label="f=0.5")
+ax.scatter(E_f_over_E_m, p1_mod_0p8, label="f=0.8")
+ax.set_yscale('log')
+ax.set_xscale('log')
+plt.xlabel("Fiber/Matrix Modulus Ratio")
+plt.ylabel("Composite/Fiber Modulus Ratio")
+plt.title("P1D")
+plt.legend()
+plt.savefig("./mod_ratio_p1d.png")
+plt.show()
+
+# P1E
+# examine range of fiber modulus from 1 to 1000
+fiber_modulus = np.linspace(1e6, 1e9, 100)
+E_f_over_E_m = np.linspace(1,1000,100)
+p1a_material.volume_fraction = 0.5
+p1_s_5 = []
+p1_s_25 = []
+p1_s_50 = []
+
+p1a_material.s_factor = 5
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_s_5.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+p1a_material.s_factor = 25
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_s_25.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+p1a_material.s_factor = 50
+for i in fiber_modulus:
+    p1a_material.fiber_modulus = i
+    p1_s_50.append( (p1a_material.get_modulus_of_composite())/(i) )
+
+ax = plt.gca()
+ax.scatter(E_f_over_E_m, p1_s_5, label="s=5")
+ax.scatter(E_f_over_E_m, p1_s_25, label="s=25")
+ax.scatter(E_f_over_E_m, p1_s_50, label="s=50")
+ax.set_yscale('log')
+ax.set_xscale('log')
+plt.xlabel("Fiber/Matrix Modulus Ratio")
+plt.ylabel("Composite/Fiber Modulus Ratio")
+plt.title("P1E")
+plt.legend()
+plt.savefig("./mod_ratio_p1e.png")
 plt.show()
 
 
 
-plt.plot(p1a_x, p1a_is_stress_30, label="s=30")
-plt.plot(p1a_x, p1a_is_stress_40, label="s=40")
-plt.plot(p1a_x, p1a_is_stress_50, label="s=50")
-plt.xlabel("x")
-plt.ylabel("Interfacial Shear Stress")
+
+
+# P2
+p2_material = ShearLagComposite()
+p2_material.matrix_modulus = 1e6
+p2_material.matrix_poissonsratio = 0.4
+p2_material.fiber_poissonsratio = 0.3
+
+# P2a
+p2a_s_3 = []
+p2a_s_30 = []
+p2_material.volume_fraction = 0.3
+
+p2_material.s_factor = 3
+for i in fiber_modulus:
+    p2_material.fiber_modulus = i
+    p2a_s_3.append( (p2_material.get_modulus_of_composite())/(i) )
+
+p2_material.s_factor = 30
+for i in fiber_modulus:
+    p2_material.fiber_modulus = i
+    p2a_s_30.append( (p2_material.get_modulus_of_composite())/(i) )
+
+# rule of mixture result
+rule_of_mix_modulus = []
+for i in fiber_modulus:
+    rule_of_mix_modulus.append((0.3*i + (1-0.3)*p2_material.matrix_modulus)/(i))
+
+# modulus ratio
+fiber_modulus = np.linspace(1e6, 1e9, 100)
+E_f_over_E_m = np.linspace(1,1000,100)
+
+ax = plt.gca()
+ax.scatter(E_f_over_E_m, p1_s_5, label="s=3")
+ax.scatter(E_f_over_E_m, p1_s_25, label="s=30")
+ax.scatter(E_f_over_E_m, rule_of_mix_modulus, label="Rule of Mixture")
+ax.set_yscale('log')
+ax.set_xscale('log')
+plt.xlabel("Fiber/Matrix Modulus Ratio")
+plt.ylabel("Composite/Fiber Modulus Ratio")
+plt.title("P2A")
 plt.legend()
-plt.savefig("./is_stress_p1a.png")
+plt.savefig("./mod_ratio_p2a.png")
+plt.show()
+
+# P2b
+p2a_s_3 = []
+p2a_s_30 = []
+p2_material.volume_fraction = 0.9
+
+p2_material.s_factor = 3
+for i in fiber_modulus:
+    p2_material.fiber_modulus = i
+    p2a_s_3.append( (p2_material.get_modulus_of_composite())/(i) )
+
+p2_material.s_factor = 30
+for i in fiber_modulus:
+    p2_material.fiber_modulus = i
+    p2a_s_30.append( (p2_material.get_modulus_of_composite())/(i) )
+
+# rule of mixture result
+rule_of_mix_modulus = []
+for i in fiber_modulus:
+    rule_of_mix_modulus.append((0.9*i + (1-0.9)*p2_material.matrix_modulus)/(i))
+
+# modulus ratio
+fiber_modulus = np.linspace(1e6, 1e9, 100)
+E_f_over_E_m = np.linspace(1,1000,100)
+
+ax = plt.gca()
+ax.scatter(E_f_over_E_m, p1_s_5, label="s=3")
+ax.scatter(E_f_over_E_m, p1_s_25, label="s=30")
+ax.scatter(E_f_over_E_m, rule_of_mix_modulus, label="Rule of Mixture")
+ax.set_yscale('log')
+ax.set_xscale('log')
+plt.xlabel("Fiber/Matrix Modulus Ratio")
+plt.ylabel("Composite/Fiber Modulus Ratio")
+plt.title("P2B")
+plt.legend()
+plt.savefig("./mod_ratio_p2b.png")
 plt.show()
---
+++ zephyr_cell.py
@@ -0,0 +1,169 @@
+# ZEPHYR Cell
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Zephyr:
+    def __init__(self) -> None:
+        self.bulk_youngs_modulus = 20e6 # Pa
+        self.bulk_pratio = 0.3
+        self.l = 0.008          # m
+        self.h = 0.022           # m
+        self.z = 0.01           # m
+        self.t1 = 0.001         # m
+        self.t2 = 0.002         # m
+        self.theta_1 = 1.047    # 60 degrees
+        self.theta_2 = 1.047    # 60 degrees
+        self.a1_lattice_modulus = 1
+        self.a2_lattice_modulus = 1
+        self.a12_lattice_poissons_ratio = 0
+        self.a21_lattice_poissons_ratio = 0
+        self.m_of_inertia_1 = 1
+        self.m_of_inertia_2 = 1
+    
+    def set_m_of_inertia(self):
+        self.m_of_inertia_1 = (self.z*self.t1**3)/(12)
+        self.m_of_inertia_2 = (self.z*self.t2**3)/(12)
+        return 0
+
+    def set_lattice_modulus(self):
+        t_over_l_ratio = (1/2)*((self.t1/self.l)**3 + (self.t2/self.l)**3)
+        # print(t_over_l_ratio)
+        E1 = self.bulk_youngs_modulus*t_over_l_ratio*(self.l/self.h)*((np.cos(self.theta_1)**2)/(np.sin(self.theta_1)))
+        self.a1_lattice_modulus = E1
+        return 0
+
+    def get_lattice_pratio(self):
+        return (1 - self.theta_1/self.theta_2)
+
+    def get_lattice_modulus(self):
+        return self.a1_lattice_modulus
+
+    def set_lattice_poissons_ratio(self):
+        return 0
+    
+    def get_a1_force_displacement(self, F1):
+        """
+        For an F1 force, get resulting a1 displacement
+        """
+        a1_sigma = F1/(self.z*self.h)
+        a1_delta = (a1_sigma*self.h*self.z*(np.sin(self.theta_1)**2)*(self.l**3))/(12*self.bulk_youngs_modulus*self.m_of_inertia_1)
+        return a1_delta
+
+    def plot_stress_and_strain(self, F_list, t1_str):
+        """
+        For a given list of Forces, find stress and strain state for a Zephyr cell
+        plot stress vs strain
+        """
+        # Testing results
+        F_test = [0,1,2,3,4,5] # N approximate from spring scale
+        epsilon_test = [0,0.072, 0.102, 0.131, 0.155, 0.168]
+        sigma_test = []
+        for i in F_test:
+            # force applied over limited area
+            sigma_value = i/(0.0008*self.z)
+            sigma_test.append(sigma_value)
+        
+        # For each force, use Young's mod and buckling equation to estimate stress and strain
+        bulk_epsilon = []
+        bulk_sigma = []
+        test_epsilon = np.linspace(0,0.25,100)
+        for i in test_epsilon:
+            bulk_epsilon.append(i)
+            bulk_sigma.append(self.bulk_youngs_modulus*i)
+        sigma = []
+        epsilon = []
+
+        for i in F_list:
+            sigma_i = i/(self.h*self.z)
+            epsilon_i = self.get_a1_force_displacement(i)
+            epsilon.append(sigma_i/self.a1_lattice_modulus)
+            sigma.append(100*sigma_i)
+
+        # Get linear lattice Young's Modulus
+        delta_sigma = sigma[2] - sigma[1]
+        delta_epsilon = epsilon[2] - epsilon[1]
+        youngs_modulus = (delta_sigma)/(delta_epsilon)
+        print(youngs_modulus)
+        plt.plot(epsilon, sigma, label="Lattice")
+        plt.plot(bulk_epsilon, bulk_sigma, label="Solid")
+        plt.scatter(epsilon_test, sigma_test, label="Experiment")
+        plt.xlabel("Strain Along a1")
+        plt.ylabel("Stress [Pa]")
+        plt.xlim([0, 0.25])
+        plt.ylim([0, 5e6])
+        plt.title("Stress and Strain in a1 Loading of ZEPHYR (t1 = {} m, t2 = 0.002 m)".format(t1_str))
+        plt.legend()
+        plt.savefig("./stress_strain1_zephyr_{}.png".format(t1_str))
+        plt.show()
+        return 0
+    
+    def plot_strains_and_poissons_ratio(self):
+        """
+        Plot a1 and a2 strains and resulting P ratio
+        """
+        L_a1 = 0.050 # m
+        L_a2 = 0.176 # m
+        test_epsilon = np.linspace(0,0.25,100)
+        bulk_epsilon2 = []
+        epsilon1 = test_epsilon
+        for i in test_epsilon:
+            bulk_epsilon2.append(self.bulk_pratio*i)
+            epsilon2 = epsilon1*self.get_lattice_pratio()
+
+        epsilon1_test = [0,0.072, 0.102, 0.131, 0.155, 0.168]
+        epsilon2_test = [0,0.001, 0.002, 0.01, 0.014, 0.035]
+        plt.plot(epsilon1, epsilon2, label="Lattice")
+        plt.plot(test_epsilon, bulk_epsilon2, label="Solid")
+        plt.scatter(epsilon1_test, epsilon2_test, label="Experiment")
+        plt.xlabel("Epsilon 1")
+        plt.ylabel("Epsilon 2")
+        plt.title("Poisson's Ratio of ZEPHYR Lattice (t1 = 1 mm, t2 = 2 mm)")
+        plt.legend()
+        plt.savefig("./a1_a2_PR_zephyr.png")
+        plt.show()
+        
+        return 0
+
+
+if __name__ == "__main__":
+    test_device = Zephyr()
+    test_device.set_lattice_modulus()
+    test_device.set_lattice_poissons_ratio()
+    test_device.set_m_of_inertia()
+
+    test_device.t1 = 0.001
+    test_device.set_lattice_modulus()
+    test_device.set_lattice_poissons_ratio()
+    test_device.set_m_of_inertia()
+    test_device.plot_stress_and_strain(F_list=[0,0.1,0.2,1,2,4,5,10,20], t1_str=str(test_device.t1))
+    test_device.plot_strains_and_poissons_ratio()
+
+    test_theta = list(np.linspace(0.5,1.4, 100))
+    modulus = []
+    for i in test_theta:
+        test_device.theta_1 = i
+        test_device.set_lattice_modulus()
+        modulus.append(80*test_device.get_lattice_modulus())
+    plt.plot(np.rad2deg(test_theta), modulus, label="ZEPHR Lattice")
+    plt.xlabel("Theta (Degrees)")
+    plt.ylabel("Young's Modulus")
+    plt.title("Comparing Theta to Young's Modulus for Standard ZEPHYR")
+    plt.legend()
+    plt.savefig("./theta_and_modulus.png")
+    plt.show()
+
+    test_device.theta_1 = 1
+
+    test_h = list(np.linspace(0.001,0.030,100))
+    modulus = []
+    for i in test_theta:
+        test_device.h = i
+        test_device.set_lattice_modulus()
+        res = modulus.append(4000*test_device.get_lattice_modulus())
+    plt.plot(test_h, modulus, label="ZEPHR Lattice")
+    plt.xlabel("h (mm)")
+    plt.ylabel("Young's Modulus")
+    plt.title("Comparing h to Young's Modulus for Standard ZEPHYR")
+    plt.legend()
+    plt.savefig("./h_and_modulus.png")
+    plt.show()
